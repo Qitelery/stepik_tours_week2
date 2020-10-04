c9abca25db2722e59717bcbfe1c49b79316d9d7d
Alexey Dronov <alexadronov@gmail.com>
2020-10-04 18:00:48 +0300

    finally version 1th week

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -20,6 +20,6 @@ from tours.views import MainView, DepartureView, TourView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', MainView.as_view()),
-    path('departure/<str:departure>/', DepartureView.as_view()),
-    path('tour/<int:id>/', TourView.as_view()),
+    path('departure/<str:depar_id>/', DepartureView.as_view()),
+    path('tour/<int:tour_id>/', TourView.as_view()),
 ]
