486fe2cd7c62cef5a41706981093f85a8968ffcb
Alexey Dronov <alexadronov@gmail.com>
2020-10-11 21:14:30 +0300

    New updates for Heroku deployment

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&c5uv_!k4_94nqh(d7gius3^xi%xezs8)_lgya+j^($vo&wdg4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
